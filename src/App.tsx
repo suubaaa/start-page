4c89bc66bcd1739a4e9a81fff8dd9c3d02dd8662
suubaaa <275861463+suubaaa@users.noreply.github.com>
2026-06-07 01:08:58 -0500

    minor changes for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Search from "./components/Search";
 export default function App() {
   return(
     <div className="relative w-screen h-screen overflow-hidden"
-      style={{ backgroundImage: "url(/background.jpg)", backgroundSize: 'cover', backgroundPosition: 'center' }}
+      style={{ backgroundImage: "url(/start-page/background.jpg)", backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
       <DateTime />
       <Links />
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/start-page/',
   plugins: [react(), tailwindcss()],
 })
